f6c061716c93db4524a951f61ed1c323770f1d52
Toshiki Kamio <41144380+tcamio@users.noreply.github.com>
2019-02-17 16:21:09 -0700

    Add files via upload

--- Lab1/Lab1_2_a.py
+++ Lab1/Lab1_2_a.py
@@ -1,7 +1,13 @@
+'''
+This program write a recursive method to draw given figures in CS2302 Lab1,
+Question 2.
+'''
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math 
 
+# Create a list of x- and y- coordination based on center and rad
 def circle(center,rad):
     n = int(4*rad*math.pi)
     t = np.linspace(0,6.3,n)
@@ -9,10 +15,18 @@ def circle(center,rad):
     y = center[1]+rad*np.cos(t)
     return x,y
 
+'''
+draw_circles draws circles recursively. The first circle is drawn based on the
+given center and radius. The consecutive squares are drawn with weighted radius
+radius*w and the x-coordination of the center is shifted -radius*(1-w).
+'''
 def draw_circles(ax,n,center,radius,w):
     if n>0:
+        # Create and draw a crcle based on given center and radius
         x,y = circle(center,radius)
         ax.plot(x,y,color='k')
+        
+        # The center of next circle is shifted and call draw_circles
         center[0]=center[0]-radius*(1-w)
         draw_circles(ax,n-1,center,radius*w,w)
       
